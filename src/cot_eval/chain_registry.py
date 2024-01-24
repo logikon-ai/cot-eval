bead7971f7def089967c31d54020fa747a7e2b13
Gregor Betz <3662782+ggbetz@users.noreply.github.com>
2024-01-24 16:00:41 +0100

    correct import

--- src/cot_eval/chain_registry.py
+++ src/cot_eval/chain_registry.py
@@ -1,7 +1,7 @@
 """Global registry of all COT chains
 """
 
-from cot_eval.chains import ReflectBeforeRun
+from cot_eval.chains.ReflectBeforeRun import ReflectBeforeRun
 
 CHAIN_REGISTRY = {
     "ReflectBeforeRun": ReflectBeforeRun,
