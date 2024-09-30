e6c2cc9c11e996ace7da6a42e3ca8b73cf58d51a
Gregor Betz <3662782+ggbetz@users.noreply.github.com>
2024-09-30 10:33:48 +0200

    register SelfCorrect

--- src/cot_eval/chain_registry.py
+++ src/cot_eval/chain_registry.py
@@ -3,8 +3,10 @@
 
 from cot_eval.chains.ReflectBeforeRun import ReflectBeforeRun
 from cot_eval.chains.HandsOn import HandsOn
+from cot_eval.chains.SelfCorrect import SelfCorrect
 
 CHAIN_REGISTRY = {
     "ReflectBeforeRun": ReflectBeforeRun,
     "HandsOn": HandsOn,
+    "SelfCorrect": SelfCorrect,
 }
